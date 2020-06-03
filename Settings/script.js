aabad6269552dd53307e176c41e5fcf378533863
kafarzada <52622175+kafarzada@users.noreply.github.com>
2020-06-04 02:17:11 +0700

    добавлен ночной режим

--- Settings/script.js
+++ Settings/script.js
@@ -3,5 +3,9 @@ let items = document.querySelectorAll(".setting__item");
 items.forEach(item => {
     item.addEventListener('click',  function(){
         this.classList.toggle("actived");
+
+        if(this.hasAttribute("lightMood")) {
+            document.body.classList.toggle("light");
+        }
     })
 })
