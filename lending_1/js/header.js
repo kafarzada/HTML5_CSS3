57a7a3e8714390724075f302ca7bc480b4a17c61
kafarzada <52622175+kafarzada@users.noreply.github.com>
2020-07-29 01:43:22 +0700

    изменил шрифт

--- lending_1/js/header.js
+++ lending_1/js/header.js
@@ -5,3 +5,15 @@ $(window).on('scroll', (e) => {
         $(".header__inner").removeClass('fixed')
     }
 })
+
+
+const navLinks = document.querySelectorAll('.nav__itemLink');
+navLinks.forEach(function(item) {
+    
+    item.addEventListener('click',  function() {
+        navLinks.forEach(function(item) {
+            item.classList.remove('active')
+        })
+        item.classList.add('active')
+    })
+})
